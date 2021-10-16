eaf7c8c662564b87f15cb37f81157b8e174c2379
selene <selene.koh.2019@scis.smu.edu.sg>
2021-10-17 00:47:10 +0800

    update course.py

--- Server/Microservice/course/course.py
+++ Server/Microservice/course/course.py
@@ -157,24 +157,14 @@ def remove_course():
 
 #Learner to view selected course during certain period 
 @app.route("/view_available_course/<string:date>", methods=['GET'])
-def get_one():
-                # check for body request
-    if not request.json:
-        return("Invalid body request."), 400
+def get_one(date):
 
-    date = request.json["date"]
     conn = mysql.connect()
     cur = conn.cursor()
     cur.execute("""SELECT * FROM course.course WHERE %s BETWEEN start_date AND end_date""", [date])
     result = cur.fetchall()
-    conn.commit()
-    cur.close()
-
 
     return jsonify(result), 203
-
-
-
 
 #update learner's or trainer's class_list status to withdraw or completed: 
 @app.route('/update_status', methods=['PUT'])
@@ -185,19 +175,17 @@ def update_status():
 
     status = request.json['status']
     emp_id = request.json['emp_id']
-    course_id = request.json['course_id']
-    class_id = request.json['class_id']
 
     conn = mysql.connect()
     cur = conn.cursor()
-    cur.execute("""UPDATE course.class_list SET status = %s, course_id = %s, class_id = %s
-                WHERE emp_id = %s """,(status, emp_id, course_id, class_id))
-    
-    result = cur.fetchall()
+    cur.execute("""UPDATE course.class_list SET status = %s
+                WHERE emp_id = %s """,
+                (status, emp_id))
+
     conn.commit()
     cur.close()
 
-    return jsonify(result), 203
+    return("Success"), 201
 
 #approve learner enrolment
 @app.route('/approve_learner', methods=['PUT'])
@@ -314,7 +302,46 @@ def ineligible_courses():
 
     return jsonify(result), 203
 
+#Learner to view selected course during certain period 
+@app.route("/view_available_course/<string:date>", methods=['GET'])
+def get_one():
+                # check for body request
+    if not request.json:
+        return("Invalid body request."), 400
 
+    date = request.json["date"]
+    conn = mysql.connect()
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM course.course WHERE %s BETWEEN start_date AND end_date""", [date])
+    result = cur.fetchall()
+    conn.commit()
+    cur.close()
+
+
+    return jsonify(result), 203
+
+#update learner's or trainer's class_list status to withdraw or completed: 
+@app.route('/update_status', methods=['PUT'])
+def update_status():
+
+    if not request.json:
+        return ("Invalid body request."),400
+
+    status = request.json['status']
+    emp_id = request.json['emp_id']
+    course_id = request.json['course_id']
+    class_id = request.json['class_id']
+
+    conn = mysql.connect()
+    cur = conn.cursor()
+    cur.execute("""UPDATE course.class_list SET status = %s, course_id = %s, class_id = %s
+                WHERE emp_id = %s """,(status, emp_id, course_id, class_id))
+    
+    result = cur.fetchall()
+    conn.commit()
+    cur.close()
+
+    return jsonify(result), 203
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
