fdcfe21d859687a95df1790739928708472be193
Kam Lee Min <leemin.kam.2019@smu.edu.sg>
2021-10-19 22:26:17 +0800

    upload materials endpoint done

--- Server/Microservice/course/course.py
+++ Server/Microservice/course/course.py
@@ -3,8 +3,11 @@ import datetime
 from flask import jsonify, Flask, request
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3305
@@ -16,8 +19,8 @@ mysql = MySQL(app, cursorclass=DictCursor)
 mysql.init_app(app)
 
 
-#HR create new course
-@app.route('/create_course', methods=['POST']) 
+# HR create new course
+@app.route('/create_course', methods=['POST'])
 def create_course():
 
     # check for body request
@@ -43,37 +46,39 @@ def create_course():
 
     return("Successfully Create Course"), 201
 
-#HR Create new class to assign trainers after creating a new course
-@app.route('/create_class', methods=['POST']) 
+# HR Create new class to assign trainers after creating a new course
+
+
+@app.route('/create_class', methods=['POST'])
 def create_class():
-    #check for body request
+    # check for body request
     if not request.json:
         return("Invalid body request."), 400
-    
-    course_id = request.json['course_id'];
-    class_id = request.json['class_id'];
-    class_name= request.json['class_name'];
-    intake = request.json['intake'];
-    emp_id = request.json['emp_id'];
+
+    course_id = request.json['course_id']
+    class_id = request.json['class_id']
+    class_name = request.json['class_name']
+    intake = request.json['intake']
+    emp_id = request.json['emp_id']
 
     conn = mysql.connect()
     cur = conn.cursor()
     cur.execute("""INSERT INTO course.class(class_id,class_name,intake,emp_id,course_id) VALUES (%s, %s, %s, %s, %s)""",
-                (class_id,class_name,intake,emp_id,course_id))    # commit the command
+                (class_id, class_name, intake, emp_id, course_id))    # commit the command
     conn.commit()
 
     # close sql connection
     cur.close()
 
     return("Successfully Create Class"), 201
-    
 
-#HR update course
-@app.route('/update_course', methods=['PUT']) 
+
+# HR update course
+@app.route('/update_course', methods=['PUT'])
 def update_class():
-    #check for body request
+    # check for body request
     if not request.json:
-        return ("Invalid body request."),400
+        return ("Invalid body request."), 400
 
     course_id = request.json['course_id']
     course_name = request.json['course_name']
@@ -85,7 +90,7 @@ def update_class():
     cur = conn.cursor()
     cur.execute("""UPDATE course.course SET course_name = %s, start_date = %s , end_date =%s, pre_req=%s
                 WHERE course_id = %s """,
-                (course_name, start_date, end_date, pre_requisite,course_id))
+                (course_name, start_date, end_date, pre_requisite, course_id))
 
     # commit the command
     conn.commit()
@@ -95,7 +100,8 @@ def update_class():
 
     return("Successfully Update Course"), 201
 
-@app.route("/all_courses", methods=['GET']) 
+
+@app.route("/all_courses", methods=['GET'])
 def all_courses():
 
     conn = mysql.connect()
@@ -107,7 +113,8 @@ def all_courses():
 
 ## learner want to enroll to the course ##
 
-@app.route('/enrol', methods=['POST']) 
+
+@app.route('/enrol', methods=['POST'])
 def enrol():
 
     # check for body request
@@ -163,9 +170,9 @@ def add_course():
     # close sql connection
     cur.close()
 
-
     return("Success"), 201
-    
+
+
 @app.route('/remove', methods=['DELETE'])
 def remove_course():
 
@@ -182,7 +189,9 @@ def remove_course():
 
     return("Success"), 202
 
-#enroll engineer 
+# enroll engineer
+
+
 @app.route('/enroll_engineer', methods=['POST'])
 def enroll_engineer():
 
@@ -204,23 +213,28 @@ def enroll_engineer():
 
     return("Success"), 201
 
-#Learner to view selected course during certain period 
+# Learner to view selected course during certain period
+
+
 @app.route("/course/<string:date>", methods=['GET'])
 def get_one(date):
 
     conn = mysql.connect()
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM course.course WHERE %s BETWEEN start_date AND end_date""", [date])
+    cur.execute(
+        """SELECT * FROM course.course WHERE %s BETWEEN start_date AND end_date""", [date])
     result = cur.fetchall()
 
     return jsonify(result), 203
 
-#update learner's or trainer's class_list status to withdraw or completed: 
+# update learner's or trainer's class_list status to withdraw or completed:
+
+
 @app.route('/update_status', methods=['PUT'])
 def update_status():
 
     if not request.json:
-        return ("Invalid body request."),400
+        return ("Invalid body request."), 400
 
     status = request.json['status']
     emp_id = request.json['emp_id']
@@ -236,7 +250,9 @@ def update_status():
 
     return("Success"), 201
 
-#approve learner enrolment
+# approve learner enrolment
+
+
 @app.route('/approve_learner', methods=['PUT'])
 def approve_learner():
 
@@ -254,11 +270,13 @@ def approve_learner():
     conn.commit()
     cur.close()
     return("Success"), 202
-  
-#as a learner & trainer, able to view course progress 
+
+# as a learner & trainer, able to view course progress
+
+
 @app.route("/learner_progress", methods=['GET'])
 def learner_progresss():
-    
+
     if not request.json:
         return("Invalid body request."), 400
 
@@ -275,10 +293,10 @@ def learner_progresss():
     return ("Success"), 205
 
 
-#as a learner, view course outline and description
+# as a learner, view course outline and description
 @app.route("/course_info", methods=['GET'])
 def course_info():
-        # check for body request
+    # check for body request
     if not request.json:
         return("Invalid body request."), 400
 
@@ -294,10 +312,12 @@ def course_info():
 
     return ("Success"), 205
 
-#as learner, view course materials
+# as learner, view course materials
+
+
 @app.route("/course_materials", methods=['GET'])
 def course_materials():
-        # check for body request
+    # check for body request
     if not request.json:
         return("Invalid body request."), 400
 
@@ -306,13 +326,13 @@ def course_materials():
     conn = mysql.connect()
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM course.course WHERE course_id=%s",(course_id))
+    cur.execute("SELECT * FROM course.course WHERE course_id=%s", (course_id))
 
     conn.commit()
     cur.close()
 
     return ("Success"), 205
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
