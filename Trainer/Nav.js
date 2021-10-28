5a7fe1e29370f2012ac6bc1f23bea69e195c7d29
rusydiah <45583428+rusydiah@users.noreply.github.com>
2021-10-28 12:10:12 +0800

    Updated Login and Trainer files

---
+++ Trainer/Nav.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BiUserCircle } from 'react-icons/bi';
+import './Trainer.css';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const emp_id = localStorage.getItem('emp_id')
+    return (
+        <div className='Nav'>
+            <div style={{width: '80%', display: 'inline-block'}}>
+                <Link to={'/t/home'}
+                    style={{textDecoration: 'none', color: 'white'}}
+                >
+                    <h3>All-In-One</h3>
+                    <h3>Learning Management System</h3>
+                </Link>
+            </div>
+            <div style={{float: 'right', marginTop:'50px'}}>
+                <div className="user_dropdown">
+                    <BiUserCircle size={40} onClick={handleClick}/>
+                    <h3 onClick={handleClick}>Employee ID: {emp_id}</h3>
+                </div>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <Link to={'/'}
+                        style={{textDecoration: 'none', color: 'black'}}
+                    >
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Link>
+                </Menu>
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default Nav
---
+++ Trainer/TrainerCourseSection.js
@@ -0,0 +1,97 @@
+import React, {useState, useEffect} from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import './Trainer.css';
+
+
+function TrainerCourseSection({name, materials, show, classID, courseID}) {
+
+    const [selectedFile, setSelectedFile] = useState("");
+    const [isFilePicked, setIsFilePicked] = useState(show);
+    const [status, setStatus] = useState("");
+
+    const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsFilePicked(true);
+	};
+
+    useEffect( () => {
+        setIsFilePicked(show)
+    }, [name])
+
+    async function handleSubmission(){
+        // showStatus()
+        const formData = new FormData();
+		formData.append('filename', selectedFile)
+        formData.append('course_id', courseID)
+        formData.append('class_id', classID)
+        formData.append('section_id', name)
+        console.log(formData)
+        try{
+          const onSubmit =
+            await axios({
+              method: 'post',
+              url: 'http://localhost:5002/upload_materials',
+              data: formData,
+            })
+            if (onSubmit.status == 200){
+                console.log(onSubmit.data)
+                showStatus()
+            }
+            return onSubmit.status
+        }
+        catch (err) {
+            console.log(err);
+            setStatus("File not uploaded");
+        }
+    };
+
+    const Input = styled('input')({
+        display: 'none',
+    });
+
+    function showStatus() {
+        alert("You have successfully uploaded " + selectedFile.name)
+        setIsFilePicked(false)
+        window.location.reload(false);
+    }
+
+
+    return (
+        <div className="section">
+            <div className='section_contents'>
+                <h2>{ name !='' && <span>Section {name} </span>}</h2>
+                <div>
+                    <a href={materials}>{materials}</a>
+                </div>
+                { name !='' &&
+                    <div className='box'>
+                        <div>
+                            <h2>{isFilePicked}</h2>
+                            {isFilePicked ? (
+                                <div>
+                                    <p>Filename: {selectedFile.name}</p>
+                                </div>
+                            ) : (
+                                <p>Select a file to show details</p>
+                            )}
+                            <label htmlFor="contained-button-file">
+                                <Input accept=".zip" id="contained-button-file" multiple type="file" onChange={changeHandler}/>
+                                <Button variant="contained" component="span">
+                                    Upload
+                                </Button>
+                            </label>
+                        </div>
+                        <div>
+                            <Button variant="contained" color="success" onClick={handleSubmission}>
+                                Submit
+                            </Button>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+export default TrainerCourseSection
