9710cc723375eb4532c42159943e8f080f5a85a5
haliza-19992 <74004727+haliza-19992@users.noreply.github.com>
2021-11-10 22:24:56 +0800

    Edit Assign TDD

--- TDD/assign.py
+++ TDD/assign.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
-    '@127.0.0.1:3306/course'
+    '@localhost:3305/course'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
@@ -12,30 +12,6 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
 CORS(app)
 db = SQLAlchemy(app)
 
-
-class Class_List(db.Model):
-    __tablename__ = 'class_list'
-    emp_id = db.Column(db.Integer, primary_key=True)
-    emp_name = db.Column(db.String(255))
-    class_id = db.Column(db.Integer)
-    progress = db.Column(db.String(255))
-    class_status = db.Column(db.String(255))
-    ungraded_result = db.Column(db.String(255))
-    graded_result = db.Column(db.String(255))
-
-    def to_dict(self):
-        """
-        'to_dict' converts the object into a dictionary,
-        in which the keys correspond to database columns
-        """
-        columns = self.__mapper__.column_attrs.keys()
-        result = {}
-        for column in columns:
-            result[column] = getattr(self, column)
-        return result
-
-    def class_id(self):
-        return self._class_id
     
 class Class_List(db.Model):
     __tablename__ = 'class_list'
@@ -58,10 +34,6 @@ class Class_List(db.Model):
             result[column] = getattr(self, column)
         return result
 
-    def class_id(self):
-        return self._class_id
-
-db.create_all()
 
 @app.route("/insert_class_list", methods=['POST'])
 def insert_class_list():
@@ -71,7 +43,7 @@ def insert_class_list():
                        'class_id','progress','class_status','ungraded_result',
                        'graded_result')):
         return jsonify({
-            "message": "Incorrect JSON object provided."
+            "message": "Please input employee id."
         }), 500
     class_list = Class_List(
         emp_id = data['emp_id'],
@@ -84,6 +56,7 @@ def insert_class_list():
     )
     
     try:
+        
         db.session.add(class_list)
         db.session.commit()
         return jsonify(class_list.to_dict()), 201
@@ -91,6 +64,7 @@ def insert_class_list():
         return jsonify({
             "message": "Unable to commit to database."
         }), 500
+        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
--- TDD/test_assign.py
+++ TDD/test_assign.py
@@ -1,7 +1,9 @@
 import unittest
 import flask_testing
 import json
-from test_assign import app, db, Class_List
+
+from sqlalchemy.sql.elements import Null
+from assign import app, db, Class_List
 
 
 class TestApp(flask_testing.TestCase):
@@ -18,10 +20,10 @@ class TestApp(flask_testing.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-        
-    assign = Class_List(emp_id=2,emp_name='Buck',class_id=1,progress='0',class_status='Ongoing',ungraded_result='0,0,0,0',graded_result='')
 
-    def test_insert_employee(self,assign):
+class TestInsertEmployee_Success(TestApp):
+    def test_insert_employee(self):
+        assign = Class_List(emp_id=13,emp_name='Buck',class_id=1,progress='0',class_status='Ongoing',ungraded_result='0,0,0,0',graded_result='')
         request_body= {
             'emp_id': assign.emp_id,
             'emp_name': assign.emp_name,
@@ -37,7 +39,7 @@ class TestApp(flask_testing.TestCase):
                                     content_type='application/json')
 
         self.assertEqual(response.json, {
-            'emp_id': 2,
+            'emp_id': 13,
             'emp_name': 'Buck',
             'class_id': 1,
             'progress': '0',
@@ -45,6 +47,30 @@ class TestApp(flask_testing.TestCase):
             'ungraded_result' : '0,0,0,0',
             'graded_result': ''
         })
+
+
+    def test_insert_class_failed(self):
+        assign = Class_List(emp_id=20,emp_name='Jessie',class_id=1,progress='0',class_status='Ongoing',ungraded_result='0,0,0,0',graded_result='')
+        db.session.add(assign)
+        db.session.commit()
+
+        request_body = {
+            'progess' : assign.progress,
+            'class_id' :assign.class_id,
+            'class_status' :assign.class_status,
+            'ungraded_result' :assign.ungraded_result,
+            'graded_result' :assign.graded_result 
+        }
+
+        response = self.client.post("/insert_class_list",
+                                    data=json.dumps(request_body),
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json, {
+            'message': 'Please input employee id.'
+        })
+
 
 
 if __name__ == '__main__':
