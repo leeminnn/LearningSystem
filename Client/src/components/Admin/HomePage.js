52785e91bf421e7f342d9c74eaf138ca6c6d985c
Kam Lee Min <leemin.kam.2019@smu.edu.sg>
2021-11-11 18:59:59 +0800

    update endpoints

--- Client/src/components/Admin/HomePage.js
+++ Client/src/components/Admin/HomePage.js
@@ -1,73 +1,84 @@
-import React, {useState, useEffect} from 'react';
-import './Style.css';
-import Nav from './Nav';
-import Button from '@mui/material/Button';
+import React, { useState, useEffect } from "react";
+import "./Style.css";
+import Nav from "./Nav";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
-function HomePage () {
-    const [classList, setClassList] = useState([])
+function HomePage() {
+  const [classList, setClassList] = useState([]);
 
-    const nextPage = (e) => {
-        localStorage.setItem('course_id', e.entry.course_id);
-        localStorage.setItem('course_name', e.entry.course_name);
-        localStorage.setItem('pre_req', e.entry.pre_req);
-    }
+  const nextPage = (e) => {
+    localStorage.setItem("course_id", e.entry.course_id);
+    localStorage.setItem("course_name", e.entry.course_name);
+    localStorage.setItem("pre_req", e.entry.pre_req);
+  };
 
-    const getClassList =() => {
-        axios.get('http://localhost:5000/all_courses')
-        .then((response) => {
-            const myList = response.data
-            setClassList(myList)
-        });
-    };
+  const getClassList = () => {
+    axios.get("http://3.18.143.100:5000/all_courses").then((response) => {
+      const myList = response.data;
+      setClassList(myList);
+    });
+  };
 
-    useEffect(() => getClassList(), [])
-    console.log(classList)
+  useEffect(() => getClassList(), []);
+  console.log(classList);
 
-    return (
-        <div>
-            <Nav/>
-            <div style={{textAlign:'center'}}>
-                <h3>Courses</h3>
+  return (
+    <div>
+      <Nav />
+      <div style={{ textAlign: "center" }}>
+        <h3>Courses</h3>
+      </div>
+      <div
+        style={{
+          margin: "0 20%",
+          alignItems: "flex-end",
+          justifyContent: "flex-end",
+          display: "flex",
+        }}
+      >
+        <Link to="/course/create" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Create Course</Button>
+        </Link>
+      </div>
+
+      {classList.map((entry) => (
+        <div className="course_box">
+          <div className="header">
+            <div className="text">
+              <b>
+                ({entry["course_id"]}) {entry["course_name"]}
+              </b>
             </div>
-            <div style={{ margin: '0 20%', alignItems:'flex-end', justifyContent:'flex-end', display:'flex'}}>
-                <Link to='/course/create'
-                    style={{textDecoration: 'none'}}
+          </div>
+          <div>
+            <div className="content">
+              <p>Pre-Requisite course: {entry["pre_req"]}</p>
+              <p>Course Description: {entry["course_desc"]}</p>
+            </div>
+            <div className="button">
+              <Link to="/course/edit" style={{ textDecoration: "none" }}>
+                <Button
+                  onClick={() => nextPage({ entry })}
+                  variant="outlined"
+                  style={{ margin: "0 30px" }}
                 >
-                    <Button variant="outlined">Create Course</Button>
-                </Link>
+                  Edit
+                </Button>
+              </Link>
+
+              <Link to="/course/classes" style={{ textDecoration: "none" }}>
+                <Button onClick={() => nextPage({ entry })} variant="contained">
+                  View Classes
+                </Button>
+              </Link>
             </div>
-
-            {classList.map(entry => (
-                <div className='course_box'>
-                    <div className='header'>
-                        <div className='text'>
-                            <b>({entry['course_id']}) {entry['course_name']}</b>
-                        </div>
-                    </div>
-                    <div>
-                        <div className='content'>
-                            <p>Pre-Requisite course: {entry['pre_req']}</p>
-                            <p>Course Description: {entry['course_desc']}</p>
-                        </div>
-                        <div className='button'>
-                            <Link to = '/course/edit'
-                            style={{textDecoration: 'none'}}>
-                                <Button onClick={() => nextPage({entry})} variant="outlined" style={{margin:'0 30px'}}>Edit</Button>
-                            </Link>
-
-                            <Link to ='/course/classes'
-                            style={{textDecoration: 'none'}}>
-                                <Button onClick={() => nextPage({entry})} variant="contained">View Classes</Button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            ))}
-
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default HomePage 
+export default HomePage;
--- Client/src/components/Trainer/TrainerClassList.js
+++ Client/src/components/Trainer/TrainerClassList.js
@@ -1,136 +1,145 @@
-import React, {useEffect, useState} from 'react';
-import Nav from './Nav';
+import React, { useEffect, useState } from "react";
+import Nav from "./Nav";
 // import '../Style.css';
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import { DataGrid } from '@mui/x-data-grid';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Autocomplete from '@mui/material/Autocomplete';
-
+import Button from "@mui/material/Button";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import { DataGrid } from "@mui/x-data-grid";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const columns = [
-{ field: 'empid', headerName: 'Employee ID', width: 180 },
-{ field: 'name', headerName: 'Name', width: 180 },
-{ field: 'progress', headerName: 'Progress', width: 180 },
-{ field: 'role', headerName: 'Role', width: 180 },
-{ field: 'status', headerName: 'Status', width: 180 }
+  { field: "empid", headerName: "Employee ID", width: 180 },
+  { field: "name", headerName: "Name", width: 180 },
+  { field: "progress", headerName: "Progress", width: 180 },
+  { field: "role", headerName: "Role", width: 180 },
+  { field: "status", headerName: "Status", width: 180 },
 ];
 
+function TrainerClassList({ match }) {
+  const [selected, setSelected] = useState([]);
+  const [classList, setClassList] = useState([]);
+  const [employeeList, setEmployeeList] = useState([]);
+  const courseID = localStorage.getItem("course_id");
+  const [totalLearners, setTotalLearners] = useState("");
+  const availLearners = localStorage.getItem("intake");
 
-function TrainerClassList( {match} ) {
-
-    const [selected, setSelected] = useState([]);
-    const [classList, setClassList] = useState([])
-    const [employeeList, setEmployeeList] = useState([])
-    const courseID = localStorage.getItem('course_id')
-    const [totalLearners, setTotalLearners] = useState('');
-    const availLearners = localStorage.getItem('intake');
-
-    async function getClassList() {
-        let data = {course_id: courseID}
-        try{
-            const onSubmit =
-            await axios({
-                method: 'post',
-                url: 'http://localhost:5001/get_learners',
-                data: data,
-              })
-              if (onSubmit.status === 200){
-                const myList = onSubmit.data
-                let temp = []
-                for (let i = 0, len = myList.length; i < len; i++){
-                    let label = myList[i].emp_name + " (" + myList[i].emp_id + ")"
-                    let emp_id = myList[i].emp_id
-                    let name = myList[i].emp_name
-                    temp.push({label: label, emp_id: emp_id, name:name})
-                }
-                setEmployeeList(temp)
-                }
-            }
-            catch (err) {
-                console.log(err);
-            }
-
-        try{
-            const onSubmit =
-               await axios({
-                method: 'post',
-                url: 'http://localhost:5000/get_class_list',
-                data: {class_id: match.params.id},
-            })
-            if (onSubmit.status === 200){
-                setTotalLearners(onSubmit.data.length)
-                console.log(onSubmit.data)
-                let tempList = onSubmit.data
-                let temp = []
-                for (let i = 0, len = tempList.length; i < len; i++){
-                    let emp_id = tempList[i].emp_id;
-                    let name = tempList[i].emp_name;
-                    let role = tempList[i].class_status;
-                    let progress = tempList[i].progress;
-                    let status = tempList[i].status;
-                    temp.push({ id: emp_id, empid: emp_id, name: name, role: role, progress: progress, status: status })
-                }
-                setClassList(temp)
-            }
-            return onSubmit.status
+  async function getClassList() {
+    let data = { course_id: courseID };
+    try {
+      const onSubmit = await axios({
+        method: "post",
+        url: "http://3.18.143.100:5001/get_learners",
+        data: data,
+      });
+      if (onSubmit.status === 200) {
+        const myList = onSubmit.data;
+        let temp = [];
+        for (let i = 0, len = myList.length; i < len; i++) {
+          let label = myList[i].emp_name + " (" + myList[i].emp_id + ")";
+          let emp_id = myList[i].emp_id;
+          let name = myList[i].emp_name;
+          temp.push({ label: label, emp_id: emp_id, name: name });
         }
-        catch (err) {
-            console.log(err);
-        }
-    };
-
-    useEffect(() => {getClassList()}, [])
-
-    const selectLearners = (ids) => {
-        const selectedIDs = new Set(ids);
-        const selectedRowData = classList.filter((row) =>
-            selectedIDs.has(row.id)
-        );
-        setSelected(selectedRowData)
+        setEmployeeList(temp);
+      }
+    } catch (err) {
+      console.log(err);
     }
 
-    return (
-        <div>
-            <Nav/>
-            <div style={{textAlign:'center'}}>
-                <h3>Class {match.params.id}</h3>
-            </div>
-            <div style={{ height: 600, width:'70%'}} className='table'>
-                <DataGrid
-                    rows={classList}
-                    columns={columns}
-                    pageSize={10}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                    onSelectionModelChange={selectLearners}
-                />
-            </div>
-            <div>
-                <h4 style={{ display: 'flex', alignItems: 'center', justifyContent:'space-evenly', marginBottom:'20px'}}>
-                    <div style={{marginRight:'10px'}}>Total Learners: {totalLearners} </div>
-                    Seat Balance: {availLearners - totalLearners}
-                </h4>
-            </div>
-        </div>
-    )
+    try {
+      const onSubmit = await axios({
+        method: "post",
+        url: "http://3.18.143.100:5000/get_class_list",
+        data: { class_id: match.params.id },
+      });
+      if (onSubmit.status === 200) {
+        setTotalLearners(onSubmit.data.length);
+        console.log(onSubmit.data);
+        let tempList = onSubmit.data;
+        let temp = [];
+        for (let i = 0, len = tempList.length; i < len; i++) {
+          let emp_id = tempList[i].emp_id;
+          let name = tempList[i].emp_name;
+          let role = tempList[i].class_status;
+          let progress = tempList[i].progress;
+          let status = tempList[i].status;
+          temp.push({
+            id: emp_id,
+            empid: emp_id,
+            name: name,
+            role: role,
+            progress: progress,
+            status: status,
+          });
+        }
+        setClassList(temp);
+      }
+      return onSubmit.status;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getClassList();
+  }, []);
+
+  const selectLearners = (ids) => {
+    const selectedIDs = new Set(ids);
+    const selectedRowData = classList.filter((row) => selectedIDs.has(row.id));
+    setSelected(selectedRowData);
+  };
+
+  return (
+    <div>
+      <Nav />
+      <div style={{ textAlign: "center" }}>
+        <h3>Class {match.params.id}</h3>
+      </div>
+      <div style={{ height: 600, width: "70%" }} className="table">
+        <DataGrid
+          rows={classList}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          onSelectionModelChange={selectLearners}
+        />
+      </div>
+      <div>
+        <h4
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            marginBottom: "20px",
+          }}
+        >
+          <div style={{ marginRight: "10px" }}>
+            Total Learners: {totalLearners}{" "}
+          </div>
+          Seat Balance: {availLearners - totalLearners}
+        </h4>
+      </div>
+    </div>
+  );
 }
 
-export default TrainerClassList
+export default TrainerClassList;
